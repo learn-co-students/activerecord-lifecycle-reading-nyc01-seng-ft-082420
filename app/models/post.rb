16c547006be4c4c9f4cd61153869fd86d04fff2e
sdonohue0918 <sdonohue0918@gmail.com>
2020-09-16 13:21:57 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+
+  # Whenever you are modifying an attribute of the model, use before_validation. If you are doing some other action, then use before_save
+#   We use before_save for actions that need to occur that aren't modifying the model itself. For example, whenever you save to the database, let's send an email to the Author alerting them that the post was just saved!
+
+# This is a perfect before_save action. It doesn't modify the model so there is no validation weirdness, and we don't want to email the user if the Post is invalid. That would be just mean! So if you had some method called email_author_about_post you would modify your Post model to look like this:
+
+before_save :email_author_about_post
+
   private
 
   def is_title_case
@@ -13,5 +22,8 @@ class Post < ActiveRecord::Base
 
   def make_title_case
     self.title = self.title.titlecase
+  end
+
+  def email_author_about_post
   end
 end
